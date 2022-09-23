492c63a438b518e6d192347df078756199480214
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-23 17:09:46 -0500

    Add loop within gameboard module that displays the array

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const gameBoard = (() => {
+  const boardArray = ['', '', '', '', '', '', '', '', ''];
+  const squares = document.querySelectorAll('.square');
+  for (let i = 0; i < boardArray.length; i++) {
+    squares[i].textContent = boardArray[i];
+  }
+  return { boardArray, squares };
+})();
