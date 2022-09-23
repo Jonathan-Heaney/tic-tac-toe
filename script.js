f8d316a15bfd3bffae3d23793b3d63ce17f39055
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-23 17:21:32 -0500

    Add displayBoard function that shows an X when you click on the square

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
-const gameBoard = (() => {
-  const boardArray = ['', '', '', '', '', '', '', '', ''];
-  const squares = document.querySelectorAll('.square');
+// const gameBoard = (() => {
+//   const boardArray = ['', '', '', '', '', '', '', '', ''];
+//   const squares = document.querySelectorAll('.square');
+//   for (let i = 0; i < boardArray.length; i++) {
+//     squares[i].textContent = boardArray[i];
+//   }
+//   return { boardArray, squares };
+// })();
+
+const boardArray = ['', '', '', '', '', '', '', '', ''];
+const squares = document.querySelectorAll('.square');
+function displayBoard() {
   for (let i = 0; i < boardArray.length; i++) {
     squares[i].textContent = boardArray[i];
   }
-  return { boardArray, squares };
-})();
+}
+
+squares.forEach((square) => {
+  square.addEventListener('click', (e) => {
+    boardArray[`${e.target.id}`] = 'x';
+    displayBoard();
+    console.log(e.target.id);
+    console.log(boardArray);
+  });
+});
